68a47d62a9810df5541bdde5d05ae6883e90ca6d
scopophobic <66241061+scopophobic@users.noreply.github.com>
2022-05-12 13:23:31 +0530

    add-better-code
    better coding for recursion

--- recursion/recursion_P1.cpp
+++ recursion/recursion_P1.cpp
@@ -21,3 +21,35 @@ int main(){
     print(n);
     return 0;
 }
+
+
+
+
+
+
+
+//better and more standard way of coding recursion
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void print(string s, int n, int i){
+    
+    if(n<i){
+        return;
+    }
+    cout<<s<<endl;
+    print(s,n,i+1);
+}
+
+
+int main(){
+    string s;
+    cin>>s;
+    int i=1,n;
+    cin>>n;
+    print(s,n,i);
+    return 0;
+}
